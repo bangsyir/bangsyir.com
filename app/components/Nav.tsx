2e7ff96d8fe3946123bc35e26d2cf47b81d3fe17
bangsyir <bangsyir@gmail.com>
2022-12-26 02:10:10 +0800

    fix nav link direction target

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,19 +3,31 @@ export default function Nav() {
     <nav className="p-2">
       <div id="logo" className="flex gap-4 items-center justify-end">
         <div>Follow me : </div>
-        <a href="https://instagram.com/teahaliasuam">
+        <a
+          href="https://instagram.com/teahaliasuam"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/instagram.svg" className="h-5 w-5" alt="" />
         </a>
-        <a href="https://www.facebook.com/abcdefghijklmnopkrstuvwxyz1234567890">
+        <a
+          href="https://www.facebook.com/abcdefghijklmnopkrstuvwxyz1234567890"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/facebook.svg" className="h-5 w-5" alt="" />
         </a>
-        <a href="https://twitter.com/bangsyir">
+        <a href="https://twitter.com/bangsyir" target="_blank" rel="noreferrer">
           <img src="/twitter.svg" className="h-5 w-5" alt="" />
         </a>
-        <a href="https://www.linkedin.com/in/syirra-fitrah-763672193/">
+        <a
+          href="https://www.linkedin.com/in/syirra-fitrah-763672193/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/linkedin.svg" className="h-5 w-5" alt="" />
         </a>
-        <a href="https://github.com/bangsyir">
+        <a href="https://github.com/bangsyir" target="_blank" rel="noreferrer">
           <img src="/github.svg" className="h-5 w-5" alt="" />
         </a>
       </div>
