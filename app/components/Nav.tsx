53bff5c955650402721cff5bca333c353a4c99f2
bangsyir <bangsyir@gmail.com>
2022-12-27 23:13:37 +0800

    fix content error "add aria-label"

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,6 +6,7 @@ export default function Nav() {
         <a
           href="https://instagram.com/teahaliasuam"
           target="_blank"
+          aria-label="link to instagram"
           rel="noreferrer"
         >
           <img src="/instagram.svg" className="h-5 w-5" alt="" />
@@ -13,21 +14,33 @@ export default function Nav() {
         <a
           href="https://www.facebook.com/abcdefghijklmnopkrstuvwxyz1234567890"
           target="_blank"
+          aria-label="link to facebook"
           rel="noreferrer"
         >
           <img src="/facebook.svg" className="h-5 w-5" alt="" />
         </a>
-        <a href="https://twitter.com/bangsyir" target="_blank" rel="noreferrer">
+        <a
+          href="https://twitter.com/bangsyir"
+          target="_blank"
+          aria-label="link to twitter"
+          rel="noreferrer"
+        >
           <img src="/twitter.svg" className="h-5 w-5" alt="" />
         </a>
         <a
           href="https://www.linkedin.com/in/syirra-fitrah-763672193/"
           target="_blank"
+          aria-label="link to linkedin"
           rel="noreferrer"
         >
           <img src="/linkedin.svg" className="h-5 w-5" alt="" />
         </a>
-        <a href="https://github.com/bangsyir" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/bangsyir"
+          target="_blank"
+          aria-label="link to github"
+          rel="noreferrer"
+        >
           <img src="/github.svg" className="h-5 w-5" alt="" />
         </a>
       </div>
