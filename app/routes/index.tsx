b37fae098b3395050297204ac921ddac20f6c118
bangsyir <bangsyir@gmail.com>
2022-12-27 22:43:40 +0800

    change subtitle

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -30,7 +30,7 @@ export default function Index() {
         <div className="text-3xl uppercase">
           Full-Stack <span className="text-green-400">Web</span> Developer
         </div>
-        <small>Find the problem, and solve it with the code.</small>
+        <small>Find the problem, and give them a solution.</small>
       </div>
       <div className="flex flex-wrap items-center justify-center gap-4 pt-6">
         <img
@@ -94,27 +94,6 @@ export default function Index() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="mt-8 border border-zinc-700 rounded-md p-4">
-        <h1 className="text-xl">Hi, I’m Syir. Nice to meet you.</h1>
-        <hr className="my-1 border-t border-zinc-700" />
-        <p className="text-justify indent-10">
-          My name is Syirra Fitrah bin Ahmad, you can call me
-          syir/bosyir/bangsyir. As a full-stack web programmer with expertise in
-          Node.js, i have a strong foundation in both front-end and back-end
-          technologies, allowing you to build complete web applications from
-          start to finish. I'm skilled in using the Node.js runtime to build
-          efficient and scalable back-end systems, and i'm a good understanding
-          of web development principles such as security, performance, and
-          accessibility. also familiar with front-end technologies such as HTML,
-          CSS, and JavaScript, and able to use these technologies to build
-          responsive and user-friendly interfaces. I'm experience working with
-          frameworks and libraries such as React, Nextjs, Remix, and comfortable
-          using them to speed up development and streamline the process of
-          building web applications. With my skills and experience, i'm able to
-          deliver high-quality web applications that provide a seamless and
-          engaging user experience to users.
-        </p>
       </div>
     </Layout>
   );
